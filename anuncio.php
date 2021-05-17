c73c132de55149c90c349bb0611ca75f20ff0be6
Damian Mediavilla <damianmediavilla@hotmail.com>
2021-05-17 02:44:36 +0200

    primer carga

---
+++ anuncio.php
@@ -0,0 +1,50 @@
+<?php include 'includes/templates/header.php'; ?>
+    <main class="contenedor seccion contenido-centrado">
+        <h1>Casa en venta frente al bosque</h1>
+        <picture>
+            <source srcset="build/img/destacada.webp" type="image/webp"> 
+            <source srcset="build/img/destacada.jpg" type="image/jpg"> 
+            <img loading="lazy" src="build/img/destacada.jpg" alt="Imagen Destacada anuncio">
+        </picture>
+        <div class="resumen-propiedad">
+            <p class="precio">$3.000.000</p>
+            <ul class="iconos-caracteristicas">
+                <li>
+                    <img src="build/img/icono_wc.svg" alt="váter">
+                    <p>4</p>
+                </li>
+                <li>
+                    <img src="build/img/icono_estacionamiento.svg" alt="garage">
+                    <p>2</p>
+                </li>
+                <li>
+                    <img src="build/img/icono_dormitorio.svg" alt="dormi">
+                    <p>6</p>
+                </li>
+
+            </ul>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis cum sint placeat ratione esse dolores consectetur veritatis quidem aperiam. Recusandae soluta aliquid ipsa omnis corrupti ex perspiciatis accusantium fuga unde!
+            Rerum exercitationem nostrum illo distinctio, adipisci quis aperiam nobis voluptate illum harum, similique in deserunt sed porro libero voluptatem! Vel quam provident adipisci laborum veniam natus autem similique, doloremque dolore!
+            A iusto totam accusantium deleniti ducimus aliquid sed commodi laboriosam numquam animi veniam possimus quaerat nihil voluptates rerum dolore magnam vel necessitatibus, soluta voluptas. Quidem dignissimos soluta distinctio pariatur vitae!
+            Nesciunt quia error dolore. Aliquid debitis placeat harum cupiditate ipsam id excepturi, ullam ducimus eos odit molestias dolor aut fugit hic accusantium earum voluptatibus quod maxime sequi et, esse nostrum.
+            Consequuntur nostrum ipsa laboriosam maxime enim atque incidunt, necessitatibus modi provident obcaecati quasi repellendus ullam voluptas quaerat praesentium adipisci, facilis, saepe nulla officia sunt asperiores illo. Quam dolor saepe cupiditate.</p>
+
+        </div>
+    </main>
+    <footer class="footer seccion">
+        <div class="contenedor contenedor-footer">
+            <nav class="navegacion">
+                <a href="nosotros.html">Nosotros</a>
+                <a href="anuncios.html">Anuncios</a>
+                <a href="blog.html">Blog</a>
+                <a href="contacto.html">Contacto </a>
+            </nav>
+
+        </div>
+        <p class="copyright">Todps los derechos reservados &copy</p>
+
+    </footer>
+</body>
+<script src="build/js/bundle.min.js"></script>
+
+</html>
---
+++ src/js/app.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function(){
+    eventListeners();
+    darkMode();
+});
+
+function darkMode(){
+    const preferenciaDarkMode=window.matchMedia('(prefers-color-scheme: dark)');
+    const botonDarkMode =document.querySelector('.dark-mode-boton');
+    botonDarkMode.addEventListener('click', function(){
+        document.body.classList.toggle('dark-mode');
+    })
+    if (preferenciaDarkMode){
+        document.body.classList.add('dark-mode');
+    }else{
+        document.body.classList.remove('dark-mode');
+    }
+    preferenciaDarkMode.addEventListener('change', function(){
+        if (preferenciaDarkMode){
+            document.body.classList.add('dark-mode');
+        }else{
+            document.body.classList.remove('dark-mode');
+        }
+
+    })
+}
+
+
+function eventListeners(){
+    const mobileMenu=document.querySelector('.mobile-menu');
+    mobileMenu.addEventListener('click', navegacionResponsive);
+}
+function navegacionResponsive(){
+    const navegacion=document.querySelector('.navegacion')
+    if(navegacion.classList.contains('mostrar')){
+        navegacion.classList.remove('mostrar');
+    }else{
+        navegacion.classList.add('mostrar');
+    }
+    //  navegacion.classList.toggle('mostrar');
+
+}
